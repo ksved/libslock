6f62b0e6603dd6028f8c1eb609dd9564815eec51
Kruthika Suresh Ved <kruthika.ved@gmail.com>
2017-10-03 17:06:23 +0200

    How much time does it take to acquire lock?

--- samples/test_ttas.c
+++ samples/test_ttas.c
@@ -1,10 +1,11 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define NUM_THREADS 4
+#define NUM_THREADS 28
 
 #include "../include/atomic_ops.h" //the memory barriers are defined there
 #include "../include/ttas.h"
+#include "time.h"
 
 ttas_lock_t* the_lock;
 
@@ -18,25 +19,38 @@ void *do_something(void *id)
     
     int count = 0;
     int i;
-    while(1){
+    int j = 10000;
+    clock_t begin, end;
+    double time_spent = 0;
+    
+    MEM_BARRIER;
+    
+    while(j > 0){
          
         for(i =0; i<10000; i++){
-            count = count*i;
+            count = count + i;
         }
+        
+        begin = clock();
         /*acquire the lock*/
         ttas_lock(the_lock,&my_data);
+        end = clock();
         
-        printf("%d\n", *my_core);
         for(i =0; i<10000; i++){
-            count = count*i;
+            count = count - i;
         }
+        
         /*release the lock*/
         ttas_unlock(the_lock);
+        
+        time_spent = time_spent + (double)(end - begin);
+        j--;
     }
     
+    time_spent = (double) time_spent /  (CLOCKS_PER_SEC * 10000);
+    printf("%d\t%lf\t%d\n", *my_core, time_spent, count);
     /*free internal memory structures which may have been allocated for the local data*/
     end_ttas_local(my_data);
-
     return NULL;
 
 }
@@ -48,7 +62,7 @@ int main(int argc, char *argv[])
      the_lock = (ttas_lock_t*)malloc(sizeof(ttas_lock_t));
     /*initialize the global data*/
     init_ttas_global(the_lock); 
-    int ids[]={0,1,2,3};
+    int ids[]= { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27 };
 
     MEM_BARRIER;
 
@@ -66,9 +80,6 @@ int main(int argc, char *argv[])
             exit(1);
         }
     }
-
-    /*free internal memory strucutres which may have been allocated for this lock */
-    end_ttas_global();
 
     pthread_exit(NULL);
 }
